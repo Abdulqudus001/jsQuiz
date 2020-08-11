eafe17314b0da141125dff57edcd889d714363e3
abdulqudus001 <abdulqudusabubakre@gmail.com>
2020-08-11 11:26:15 +0100

    Added redirect url

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,10 @@ Vue.use(VueRouter);
 
 const routes = [
   {
+    path: '/',
+    redirect: 'Login',
+  },
+  {
     path: '/home',
     name: 'Home',
     component: () => import('@/views/Home.vue'),
